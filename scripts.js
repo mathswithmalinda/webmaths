858e5b3d12089b412179a3f294f22cab8c137882
mathswithmalinda <malindak813@gmail.com>
2025-07-22 00:55:23 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,26 @@
+/*!
+* Start Bootstrap - Personal v1.0.1 (https://startbootstrap.com/template-overviews/personal)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-personal/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+
+
+document.getElementById("courseBtn").onclick  = function(){
+    window.location.href = "log.html";
+   // window.alert("success")
+
+};
+
+
+
+
+
+
+
+document.getElementById("aboutBtn").onclick  = function(){
+    window.location.href = "about.html";
+
+};
+
